63a12044d32b13467c2de7d26a5e986085c60722
Vladchere <vladchere.ok@mail.ru>
2020-06-23 18:12:24 +0700

    Initial Commit

---
+++ AlamofireApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireApp
+//
+//  Created by Vladislav on 23.06.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
